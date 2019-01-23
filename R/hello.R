fa8a17066324ff283917a71fee0212366cea27ca
Shane Orr <shanej.orr@gmail.com>
2019-01-23 16:30:29 -0500

    fix spelling of continuous

--- R/hello.R
+++ R/hello.R
@@ -15,16 +15,16 @@
 hello <- function() {
   print("Hello, world!")
 }
-
-# df <- data.frame(year = c(2016, 2017),
-#                  geo_description = c('Forsyth County, NC', 'Guilford County, NC'),
-#                  estimate = c(1,2),
-#                  se = c(.2, .3),
-#                  success = c(10, 12),
-#                  trials = c(15, 19))
 #
-# ff_sigtest(data_frame = df, estimate = 'estimate', se = 'se',
-#            test = 'zscore', var_names = c('year', 'geo_description')) %>%
-#   # Pipe significance testing matrix into function creating kable table
-#   ff_sigtest_kable(sigtest_matrix = ., test = 'zscore', table_name = 'Example table')
+# df <- data.frame(year = c(2016, 2016, 2017, 2017),
+#                  geo_description = c('Forsyth County, NC', 'Guilford County, NC',
+#                                      'Forsyth County, NC', 'Guilford County, NC'),
+#                  estimate = c(.66, .63, .88, .48),
+#                  success = c(10, 12, 15, 19),
+#                  trials = c(15, 19, 17, 39))
+#
+#
+# ff_estimates_ci(df, 'estimate', format = 'binomial',
+#                 success = 'success', trials = 'trials', var_names = c('year', 'geo_description'),
+#                 pretty_print = TRUE)
 
