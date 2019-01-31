dccc1f9fea9e84c3930bf9b9dfc0c7410173c4a3
Shane Orr <shanej.orr@gmail.com>
2019-01-31 10:43:31 -0500

    Create crosswalk dataset for use in package

---
+++ data-raw/county_cw.R
@@ -0,0 +1,21 @@
+######################################################################################
+#
+# This script creates an R object for a crosswalk of county FIPS codes to counyt names
+#
+#######################################################################################
+library(tidyverse)
+
+cw <- read_csv('data-raw/puma_counties_cw.csv') %>%
+  select(county, cntyname) %>%
+  # county FIPS codes have state and county numbers;
+  # extract last two numbers, which represent county numbers
+  mutate(county = as.integer(str_extract(county, '[0-9]{2}$')),
+         # county names do not have a comma between county name and NC
+         # and do not include words 'county'; add these back
+         cntyname = str_replace(cntyname, " NC$", " County, NC")) %>%
+  # remove duplicates
+  distinct() %>%
+  rename(county_fips = county, county_name = cntyname)
+
+# write out dataset, so it can be used in the packages
+usethis::use_data(cw, internal = TRUE)
