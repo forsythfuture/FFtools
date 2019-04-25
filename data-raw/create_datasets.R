92c54ea5f1dce29b6e0732653dcf46aba008e6e2
shanejorr <shane.j.orr@gmail.com>
2019-04-25 21:03:46 +0000

    add function to import from acs data

---
+++ data-raw/create_datasets.R
@@ -0,0 +1,25 @@
+######################################################################################
+#
+# This script creates and saves datasets that will be used internall in functions
+#
+#######################################################################################
+library(tidyverse)
+
+# Create an R object for a crosswalk of county FIPS codes to counyt names
+cw <- read_csv('data-raw/puma_counties_cw.csv') %>%
+  select(county, cntyname) %>%
+  # county FIPS codes have state and county numbers;
+  # extract last two numbers, which represent county numbers
+  mutate(county = as.integer(str_extract(county, '[0-9]{3}$')),
+         # county names do not have a comma between county name and NC
+         # and do not include words 'county'; add these back
+         cntyname = str_replace(cntyname, " NC$", " County, NC")) %>%
+  # remove duplicates
+  distinct() %>%
+  rename(county_fips = county, county_name = cntyname)
+
+# create dataset of acs variablenames
+acs_variables <- read_csv('data-raw/acs_variable_names.csv')
+
+# write out dataset
+usethis::use_data(cw, acs_variables, internal = TRUE, overwrite = TRUE)
